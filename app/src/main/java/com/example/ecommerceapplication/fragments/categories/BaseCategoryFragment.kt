81bef3f275ff250d95c91773ddd8b2b7aadb937c
kunal <kunal.dwarka@gmail.com>
2023-04-11 08:00:49 +0530

    Finished Categories

--- app/src/main/java/com/example/ecommerceapplication/fragments/categories/BaseCategoryFragment.kt
+++ app/src/main/java/com/example/ecommerceapplication/fragments/categories/BaseCategoryFragment.kt
@@ -4,17 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.ecommerceapplication.R
 import com.example.ecommerceapplication.adapters.BestProductsAdapter
 import com.example.ecommerceapplication.databinding.FragmentBaseCategoryBinding
 
 open class BaseCategoryFragment: Fragment(R.layout.fragment_base_category) {
     private lateinit var binding: FragmentBaseCategoryBinding
-    private lateinit var offerAdapter: BestProductsAdapter
-    private lateinit var bestProductsAdapter: BestProductsAdapter
+    protected val offerAdapter: BestProductsAdapter by lazy { BestProductsAdapter() }
+    protected val  bestProductsAdapter: BestProductsAdapter by lazy { BestProductsAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,10 +32,33 @@ open class BaseCategoryFragment: Fragment(R.layout.fragment_base_category) {
 
         setupOfferRv()
         setupBestProductsRv()
+
+        binding.rvOfferProducts.addOnScrollListener(object : RecyclerView.OnScrollListener(){
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+
+                if (!recyclerView.canScrollVertically(1) && dx != 0){
+                    onOfferPagingRequest()
+                }
+            }
+        })
+
+        binding.nestedScrollBaseCategory.setOnScrollChangeListener(NestedScrollView.OnScrollChangeListener{ v, _, scrollY, _, _ ->
+            if (v.getChildAt(0).bottom <= v.height + scrollY){
+                onBestProductsPagingRequest()
+            }
+        })
+    }
+
+    open fun onOfferPagingRequest(){
+
+    }
+
+    open fun onBestProductsPagingRequest(){
+
     }
 
     private fun setupBestProductsRv() {
-        bestProductsAdapter = BestProductsAdapter()
         binding.rvBestProducts.apply {
             layoutManager = GridLayoutManager(requireContext(),2, LinearLayoutManager.VERTICAL,false)
             adapter = bestProductsAdapter
@@ -41,10 +66,25 @@ open class BaseCategoryFragment: Fragment(R.layout.fragment_base_category) {
     }
 
     private fun setupOfferRv() {
-        offerAdapter = BestProductsAdapter()
-        binding.rvBestProducts.apply {
+        binding.rvOfferProducts.apply {
             layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.HORIZONTAL,false)
             adapter = offerAdapter
         }
+    }
+
+    fun showOfferLoading(){
+        binding.offerProductsProgressBar.visibility = View.VISIBLE
+    }
+
+    fun hideOfferLoading(){
+        binding.offerProductsProgressBar.visibility = View.GONE
+    }
+
+    fun showBestProductsLoading(){
+        binding.bestProductsProgressBar.visibility = View.VISIBLE
+    }
+
+    fun hideBestProductsLoading(){
+        binding.bestProductsProgressBar.visibility = View.GONE
     }
 }
--- app/src/main/java/com/example/ecommerceapplication/fragments/categories/ChairFragment.kt
+++ app/src/main/java/com/example/ecommerceapplication/fragments/categories/ChairFragment.kt
@@ -1,4 +1,70 @@
 package com.example.ecommerceapplication.fragments.categories
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.example.ecommerceapplication.data.Category
+import com.example.ecommerceapplication.utils.Resource
+import com.example.ecommerceapplication.viewmodel.CategoryViewModel
+import com.example.ecommerceapplication.viewmodel.factory.BaseCategoryViewModelFactoryFactory
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import javax.inject.Inject
+
+@AndroidEntryPoint
 class ChairFragment: BaseCategoryFragment() {
+
+    @Inject
+    lateinit var firestore: FirebaseFirestore
+
+    val viewModel by viewModels<CategoryViewModel> {
+        BaseCategoryViewModelFactoryFactory(firestore, Category.Chair)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.offerProducts.collectLatest {
+                when (it) {
+                    is Resource.Loading -> {
+                        showOfferLoading()
+                    }
+                    is Resource.Success -> {
+                        offerAdapter.differ.submitList(it.data)
+                        hideOfferLoading()
+                    }
+                    is Resource.Error -> {
+                        Snackbar.make(requireView(), it.message.toString(), Snackbar.LENGTH_LONG)
+                            .show()
+                        hideOfferLoading()
+                    }
+                    else -> Unit
+                }
+            }
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.bestProducts.collectLatest {
+                when (it) {
+                    is Resource.Loading -> {
+                        showBestProductsLoading()
+                    }
+                    is Resource.Success -> {
+                        bestProductsAdapter.differ.submitList(it.data)
+                        hideBestProductsLoading()
+                    }
+                    is Resource.Error -> {
+                        Snackbar.make(requireView(), it.message.toString(), Snackbar.LENGTH_LONG)
+                            .show()
+                        hideBestProductsLoading()
+                    }
+                    else -> Unit
+                }
+            }
+        }
+    }
 }
