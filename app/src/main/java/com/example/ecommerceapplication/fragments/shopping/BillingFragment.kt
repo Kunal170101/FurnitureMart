bb7995151bea1ced1ca4f86a70057b21c1aa5998
kunal <kunal.dwarka@gmail.com>
2023-04-17 13:48:45 +0530

    Billing Fragment

---
+++ app/src/main/java/com/example/ecommerceapplication/fragments/shopping/BillingFragment.kt
@@ -0,0 +1,102 @@
+package com.example.ecommerceapplication.fragments.shopping
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.ecommerceapplication.R
+import com.example.ecommerceapplication.adapters.AddressAdapter
+import com.example.ecommerceapplication.adapters.BillingProductsAdapter
+import com.example.ecommerceapplication.data.CartProduct
+import com.example.ecommerceapplication.databinding.FragmentBiliingBinding
+import com.example.ecommerceapplication.utils.HorizontalItemDecoration
+import com.example.ecommerceapplication.utils.Resource
+import com.example.ecommerceapplication.viewmodel.BillingViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+
+@AndroidEntryPoint
+class BillingFragment: Fragment() {
+
+    private lateinit var binding: FragmentBiliingBinding
+    private val addressAdapter by lazy { AddressAdapter() }
+    private val billingProductsAdapter by lazy { BillingProductsAdapter() }
+    private val billingViewModel by viewModels<BillingViewModel>()
+    private val args by navArgs<BillingFragmentArgs>()
+    private var products = emptyList<CartProduct>()
+    private var totalPrice = 0f
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        products = args.products.toList()
+        totalPrice = args.totalPrice
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentBiliingBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupBillingProductsRv()
+        setupAddressRv()
+
+        binding.imageAddAddress.setOnClickListener {
+            findNavController().navigate(R.id.action_billingFragment_to_addressFragment)
+        }
+
+        lifecycleScope.launchWhenStarted {
+            billingViewModel.address.collectLatest {
+                when (it) {
+                    is Resource.Loading -> {
+                        binding.progressbarAddress.visibility = View.VISIBLE
+                    }
+                    is Resource.Success -> {
+                        addressAdapter.differ.submitList(it.data)
+                        binding.progressbarAddress.visibility = View.GONE
+                    }
+                    is Resource.Error -> {
+                        binding.progressbarAddress.visibility = View.GONE
+                        Toast.makeText(requireContext(), "Error ${it.message}", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                    else -> Unit
+                }
+            }
+        }
+
+        billingProductsAdapter.differ.submitList(products)
+        binding.tvTotalPrice.text = "$ $totalPrice"
+    }
+
+    private fun setupAddressRv() {
+        binding.rvAddress.apply {
+            layoutManager = LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, false)
+            adapter = addressAdapter
+            addItemDecoration(HorizontalItemDecoration())
+        }
+    }
+
+    private fun setupBillingProductsRv() {
+        binding.rvProducts.apply {
+            layoutManager = LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, false)
+            adapter = billingProductsAdapter
+            addItemDecoration(HorizontalItemDecoration())
+        }
+    }
+}
