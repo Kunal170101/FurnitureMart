a5a92b72bb3558f197cc150d468dade40e813d0c
kunal <kunal.dwarka@gmail.com>
2023-04-21 11:16:22 +0530

    Navigations and User settings

--- app/src/main/java/com/example/ecommerceapplication/fragments/shopping/ProfileFragment.kt
+++ app/src/main/java/com/example/ecommerceapplication/fragments/shopping/ProfileFragment.kt
@@ -1,7 +1,98 @@
 package com.example.ecommerceapplication.fragments.shopping
 
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
+import com.example.ecommerceapplication.BuildConfig
 import com.example.ecommerceapplication.R
+import com.example.ecommerceapplication.activities.LoginRegisterActivity
+import com.example.ecommerceapplication.databinding.FragmentProfileBinding
+import com.example.ecommerceapplication.utils.Resource
+import com.example.ecommerceapplication.utils.showBottomNavigationView
+import com.example.ecommerceapplication.viewmodel.ProfileViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
+@AndroidEntryPoint
 class ProfileFragment: Fragment(R.layout.fragment_profile) {
+
+    private lateinit var binding: FragmentProfileBinding
+    val viewModel by viewModels<ProfileViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentProfileBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.constraintProfile.setOnClickListener {
+            findNavController().navigate(R.id.action_profileFragment_to_userAccountFragment)
+        }
+
+        binding.linearAllOrders.setOnClickListener {
+            findNavController().navigate(R.id.action_profileFragment_to_ordersFragment)
+        }
+
+        binding.linearBilling.setOnClickListener {
+            val action = ProfileFragmentDirections.actionProfileFragmentToBillingFragment(
+                0f,
+                emptyArray()
+            )
+            findNavController().navigate(action)
+        }
+
+        binding.linearLogOut.setOnClickListener {
+            viewModel.logout()
+            val intent = Intent(requireActivity(), LoginRegisterActivity::class.java)
+            startActivity(intent)
+            requireActivity().finish()
+        }
+
+        binding.tvVersion.text = "Version ${BuildConfig.VERSION_CODE}"
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.user.collectLatest {
+                when (it) {
+                    is Resource.Loading -> {
+                        binding.progressbarSettings.visibility = View.VISIBLE
+                    }
+                    is Resource.Success -> {
+                        binding.progressbarSettings.visibility = View.GONE
+                        Glide.with(requireView()).load(it.data!!.imgPath).error(
+                            ColorDrawable(
+                            Color.BLACK)
+                        ).into(binding.imageUser)
+                        binding.tvUserName.text = "${it.data.firstName} ${it.data.lastName}"
+                    }
+                    is Resource.Error -> {
+                        Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT).show()
+                        binding.progressbarSettings.visibility = View.GONE
+                    }
+                    else -> Unit
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        showBottomNavigationView()
+    }
 }
